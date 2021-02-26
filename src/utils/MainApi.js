fda14401d0f020f288dcc97daab8740865288c8c
Zhanna Kalinkina <kalinkinajane@yandex.ru>
2021-02-26 23:42:58 +0200

    исправлены ошибки в апи

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,7 +1,5 @@
 // const BASE_URL = 'https://localhost:3000'
-const BASE_URL = `${window.location.protocol}${
-  process.env.REACT_APP_API_URL || "//localhost:3000"
-}`;
+const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3000'}`
 const checkResponse = (res) =>
   new Promise((resolve, reject) => {
     const func = res.status < 400 ? resolve : reject;
@@ -12,9 +10,9 @@ export const getUser = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: "GET",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
+      "Authorization": `Bearer ${token}`,
     },
   }).then((res) => {
     if (res.ok) {
@@ -27,7 +25,7 @@ export const register = (password, email, name) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email, name }),
@@ -44,7 +42,7 @@ export const authorize = (password, email) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email }),
@@ -59,7 +57,7 @@ export const createArticle = (data) => {
   return fetch(`${BASE_URL}/articles`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json",
       Authorization: `Bearer ${localStorage.getItem("jwt")}`,
     },
@@ -83,9 +81,9 @@ export const getArticles = () => {
   return fetch(`${BASE_URL}/articles`, {
     method: "GET",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
     },
   }).then((res) => {
     if (res.ok) {
@@ -98,9 +96,9 @@ export const deleteArticles = (id) => {
   return fetch(`${BASE_URL}/articles/${id}`, {
     method: "DELETE",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
     },
   }).then((res) => {
     if (res.ok) {
